16e4fc1ff1b0a7070deab4bd2a4b508a6a814a6a
Ally-3 <146335458+Ally-3@users.noreply.github.com>
2023-12-13 11:00:42 +0000

    show status' are working

--- src/components/gameStatus/showPlaying.jsx
+++ src/components/gameStatus/showPlaying.jsx
@@ -1,18 +1,16 @@
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // Creating a functional component named RegisterForm
 function ShowPlaying(props) {
     const [playingGames, setPlayingGames] = useState([]);
-    const [games, setGames] = useState([]);
-    const [searchResults, setSearchResults] = useState();
+    const [searchResults, setSearchResults] = useState(null);
     const [resultsArray, setResultsArray] = useState([]);
     const navigate = useNavigate();
 
     // Function to send registration data to the backend
-    async function sendShowStatusFromBackend(email, setPlayingGames, setLoggedIn, setRegistered) {
+    async function sendShowStatusFromBackend(email, setLoggedIn, setRegistered) {
         try {
-            console.log(process.env.REACT_APP_BASE_URL);
             const response = await fetch(
                 `${process.env.REACT_APP_BASE_URL}/showPlaying`,
                 {
@@ -23,48 +21,21 @@ function ShowPlaying(props) {
                     })
                 }
             );
-            
+
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-    
-            const data = await response.json().catch(error => {
-                console.error('Error parsing JSON:', error.message);
-             });
-             console.log(data);
 
-             var myArray2 = [];
+            const data = await response.json();
+            console.log(data);
 
-             var myArray = data.details;
-             console.log(myArray);
-             console.log(data);
-             let x = 
-             await Promise.all(myArray.map((item, index) => {
-                console.log(item.gameID)
-                getGameDataByID(item.gameID)
+            const gameIds = data.details.map(item => item.gameID);
 
-                 console.log(searchResults)
-                 myArray2.push(searchResults)
-                 return myArray2
-             }))
-             console.log(x);
+            const gameDataArray = await Promise.all(gameIds.map((gameID) => getGameDataByID(gameID)));
 
-             var myArray3 = []
+            setResultsArray(gameDataArray);
 
-             for (let i = 0; i < myArray.length; i++) {
-                let x = getGameDataByID (myArray[i].gameID);
-                console.log(x);
-                myArray3.push(x);
-             }
-             console.log(myArray3);
-             setResultsArray (myArray3);
-
-
-
-            setPlayingGames(data.details, () => {
-                // Code to run after state has been updated
-                console.log('Playing games state updated:', playingGames);
-            });
+            setPlayingGames(data.details);
 
             // Setting loggedIn and registered states
             setLoggedIn(true);
@@ -79,32 +50,29 @@ function ShowPlaying(props) {
         try {
             const res = await fetch(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_RAWG_KEY}`);
             const data = await res.json();
-            
+            // Handle data as needed
         } catch (error) {
             console.error('Error fetching games:', error.message);
         }
     }
+
     async function getGameDataByID(searchID) {
         try {
-        console.log(searchID);    
-        const url = `https://api.rawg.io/api/games/${searchID}`+`?key=${process.env.REACT_APP_RAWG_KEY}`
-        console.log(url);
-        const res4 = await fetch(url);
-        const data4 = await res4.json();
-        console.log(data4);
-        setSearchResults(data4);
-        return data4;
+            const url = `https://api.rawg.io/api/games/${searchID}?key=${process.env.REACT_APP_RAWG_KEY}`;
+            const res4 = await fetch(url);
+            const data4 = await res4.json();
+            console.log(data4);
+            setSearchResults(data4);
+            return data4;
         } catch (error) {
             console.error('Error fetching gameID:', error.message);
         }
-        
     }
 
     useEffect(() => {
         fetchGames();
-        sendShowStatusFromBackend(props.email, setPlayingGames, props.setLoggedIn, props.setRegistered);
-     }, [props.email]);
-     
+        sendShowStatusFromBackend(props.email, props.setLoggedIn, props.setRegistered);
+    }, [props.email]);
 
     useEffect(() => {
         // Log the updated state here
@@ -118,9 +86,9 @@ function ShowPlaying(props) {
                     <h1>Games Playing: </h1>
                 </div>
                 <div className="horizontal-fade-line"></div>
-    
+
                 <div className="gamesContainer">
-                    {(resultsArray) ? (
+                    {resultsArray.length > 0 ? (
                         resultsArray.map((game) => (
                             <div className="gameItem" key={game.id}>
                                 <img className="gameImage" src={game.background_image} alt={game.name} />
--- src/pages/MyGames.jsx
+++ src/pages/MyGames.jsx
@@ -2,17 +2,48 @@ import React, { useState } from 'react';
 import '../components/navbar.css';
 import '../pages/gamesLibrary.css';
 import ShowPlaying from "../components/gameStatus/showPlaying";
+import ShowCompleted from '../components/gameStatus/showCompleted';
+import ShowOwned from "../components/gameStatus/showOwned";
+import ShowWant from "../components/gameStatus/showWant";
 
 function MyGames(props) {
     const [playingGames, setPlayingGames] = useState("");
+    const [completedGames, setCompletedGames] = useState("");
+    const [ownedGames, setOwnedGames] = useState("");
+    const [wantedGames, setWantedGames] = useState("");
 
     return (
         <div>
             <div className="headerSection">
                 <h1>My Games</h1>
+            </div >
+                <div class="horizontal-fade-line"></div>
+            <div>
+            <ShowPlaying 
+                email={props.email} 
+                playingGames={playingGames} 
+                setLoggedIn={props.setLoggedIn} 
+                setRegistered={props.setRegistered}
+            />
+            <ShowCompleted 
+                email={props.email} 
+                completedGames={completedGames} 
+                setLoggedIn={props.setLoggedIn} 
+                setRegistered={props.setRegistered}
+            />
+            <ShowOwned
+                email={props.email} 
+                ownedGames={ownedGames} 
+                setLoggedIn={props.setLoggedIn} 
+                setRegistered={props.setRegistered}
+            />
+            <ShowWant
+                email={props.email} 
+                wantedGames={wantedGames} 
+                setLoggedIn={props.setLoggedIn} 
+                setRegistered={props.setRegistered}
+            />
             </div>
-            <div class="horizontal-fade-line"></div>
-            <ShowPlaying email={props.email} playingGames={playingGames} setLoggedIn={props.setLoggedIn}/>
         </div>
     )
 };
